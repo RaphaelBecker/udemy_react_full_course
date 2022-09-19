3a2b505cf05a0a25fbc94b6d6a44db21a9f6878f
RaphaelBecker <raphael.becker.private@outlook.de>
2022-09-19 18:01:16 +0200

    added layout component

--- 02-meetup-app/src/components/meetups/MeetupItem.js
+++ 02-meetup-app/src/components/meetups/MeetupItem.js
@@ -1,20 +1,25 @@
 import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card";
 
-// Pass object data into this component by using props to make it reusabel and dynamic
+// * Pass object data into this component by using props to make it reusabel and dynamic
+// * Passing content into Card directive which is picked up by props.child in Card component
+// * wrapp css around directives with help of divs
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3> {props.title} </h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div>
-        <button className={classes.actions}>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3> {props.title} </h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
