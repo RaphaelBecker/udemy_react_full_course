7fea03a15efe62c3c9087e1f155f151a1c63db34
RaphaelBecker <raphael.becker.private@outlook.de>
2022-09-20 16:33:34 +0200

    added a submit form but not connected to a server yet

--- 02-meetup-app/src/pages/NewMeetup.js
+++ 02-meetup-app/src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-  return <div> New Meetups Page</div>;
+  return (
+    <section>
+      <h1>Add new meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
