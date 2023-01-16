eeb19024d643bd0c29874f884afd2a945b6bd359
RaphaelBecker <raphael.becker.private@outlook.de>
2023-01-16 16:04:16 +0100

    added styled component button

--- 04-starting-project/src/components/UI/Button/Button.js
+++ 04-starting-project/src/components/UI/Button/Button.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import styled from "styled-components";
 
-import './Button.css';
+// A Styled Component with scope in this file only, called tagged tamplate: with backticks ``!
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// Same button but with react JSX code, scpoe is the whole project as it uses the className="button":
+
+//const Button = (props) => {
+//  return (
+//    <button type={props.type} className="button" onClick={props.onClick}>
+//      {props.children}
+//    </button>
+//  );
+//};
 
 export default Button;
