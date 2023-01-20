e1a9020f6942b7d90f6444fba804ff47012d770e
RaphaelBecker <raphael.becker.private@outlook.de>
2023-01-20 16:53:23 +0100

    intrudced useEffect

--- 07-more_advanced_conceps/src/App.js
+++ 07-more_advanced_conceps/src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // When useEffect executes? useEffect runs after the whole component was executed!
+  // useEffect(anonymous arrow function: ()=>{} which gets executed whenever the dependency changes, [Array of dependencies]):
+  // Explanantion: We do have an empty array of dependecies, therefor it will only run once at app start!
+  useEffect(() => {
+    const storedLoggedInUserInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedLoggedInUserInfo === "LOGGED_IN") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
+    // localStorage can be acceses in the Browser Dev Tools in Tab Application
+    localStorage.setItem("isLoggedIn", "LOGGED_IN");
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
@@ -15,6 +28,7 @@ function App() {
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
   };
 
   return (
